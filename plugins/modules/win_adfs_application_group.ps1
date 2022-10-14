90c5c0a6386972aae56bfa32b88c8cc65b10ac51
Strobel, Dustin (dstrobel) <dustin.strobel@breuninger.de>
2022-10-14 15:31:55 +0200

    Add contributor to copyright

--- plugins/modules/win_adfs_application_group.ps1
+++ plugins/modules/win_adfs_application_group.ps1
@@ -1,6 +1,6 @@
 #!powershell
 
-# Copyright: (c) 2022, Dustin Strobel (@d-strobel)
+# Copyright: (c) 2022, Dustin Strobel (@d-strobel), Yasmin Hinel (@seasxlticecream)
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 #AnsibleRequires -CSharpUtil Ansible.Basic
